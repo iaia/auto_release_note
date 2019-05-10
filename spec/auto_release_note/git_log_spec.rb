f89a1aa7f65ebca6686047ec75eb494186364ee3
iaia <iaia72160@gmail.com>
2019-05-10 18:35:24 +0900

    他のバリエーションも試す

--- spec/auto_release_note/git_log_spec.rb
+++ spec/auto_release_note/git_log_spec.rb
@@ -20,7 +20,11 @@ RSpec.describe GitLog do
       Commit.new("30fde4a2 Merge pull request #456 from hotfix/654"),
       Commit.new("430cf2f4 Merge pull request #567 from release/543"),
       Commit.new("f24bd8ae Merge pull request #678 from foo"),
-      Commit.new("cdf82f34 Merge pull request #789 from bar")
+      Commit.new("cdf82f34 Merge pull request #789 from bar"),
+      Commit.new("cdf82f34 Merge pull request #1000 from feature/1000"),
+      Commit.new("cdf83f35 Merge pull request #1001 from feature/1000-2"),
+      Commit.new("cdf84f35 Merge pull request #1101 from iaia/fix/1100-bug"),
+      Commit.new("cdf85f35 Merge pull request #1102 from iaia/fix/1101-bug-2")
     ]
   end
 
@@ -32,15 +36,17 @@ RSpec.describe GitLog do
     it 'mergeログがパースできる' do
       log = subject
       expect(log.logs.map {|l| l[:pull_req_id] }).to match_array \
-        ['123', '234', '345', '456', '567', '678', '789']
+        ['123', '234', '345', '456', '567', '678', '789', '1000', '1001', '1101', '1102']
       expect(log.logs.map {|l| l[:branch] }).to match_array \
-        ['feature/987', 'fix/876', 'bug-fix/765', 'hotfix/654', 'release/543', 'foo', 'bar']
+        ['feature/987', 'fix/876', 'bug-fix/765', 'hotfix/654', 'release/543',
+         'foo', 'bar', 'feature/1000', 'feature/1000-2', 'iaia/fix/1100-bug', 'iaia/fix/1101-bug-2']
     end
 
     it 'branch名からissue idがパースできる' do
       log = subject
+      # この段階ではissue idは重複していて正しい
       expect(log.logs.map {|l| l[:issue] }).to match_array \
-        ['987', '876', '765', '654', '543', '678', '789']
+        ['987', '876', '765', '654', '543', '678', '789', '1000', '1000', '1100', '1101']
     end
   end
 
